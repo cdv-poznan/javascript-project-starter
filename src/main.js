b932a895664059303b448571eec684e84846bb13
Adrian Juszczak <adrian@juszczak.io>
2020-04-26 13:49:06 +0200

    Routing
    Signed-off-by: Adrian Juszczak <adrian@juszczak.io>

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
-import { camelCase } from 'lodash';
+// import { camelCase } from 'lodash';
 import $ from 'jquery';
+import { camelCaseView } from './views/camelcase';
+
+function enableRouting() {
+  function setRoute() {
+    $('.view').hide(); // hide - metoda jQuery
+    const { hash } = window.location; // destrukturyzacja globalnego obiektu window.location
+    // jeśli hash jest pusty
+    if (hash === '') {
+      $('#home').show(); // pokaż element o #home
+    }
+    $(hash).show(); // show - metoda jQuery
+  }
+  setRoute(); // wywołaj niezwłocznie setRoute po wywołanie enableRouting
+  window.addEventListener('hashchange', setRoute); // wywołaj setRoute na każdy event `hashchange`
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  $('#convert').click(() => {
-    const input = $('#myinput').val();
-    $('#myoutput').val(camelCase(input));
-  });
+  camelCaseView();
+  enableRouting();
 });
