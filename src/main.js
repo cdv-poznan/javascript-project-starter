a0fe178e32d8bd8531b830a3987d51e0252ccce8
KrystianDerdzinski <derdzinski.krystian@gmail.com>
2020-04-28 21:38:01 +0200

    Visual grid, basic event listener

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {});
+// import lodash from 'lodash';
+
+document.addEventListener('DOMContentLoaded', () => {
+  function checkSquare(element) {
+    this.classList.add('checked');
+  }
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(function (item) {
+    item.addEventListener('click', checkSquare);
+  });
+});
