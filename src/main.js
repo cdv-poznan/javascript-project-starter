86c4c885aae9adce29dcd7088aaa5565871f2dd8
InternetProjects-pl <biuro@internetprojects.pl>
2020-05-06 23:20:22 +0200

    porzadki po zajeciach

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {});
+import $ from 'jquery';
+import { camelCaseView } from './views/camelcase';
+import { todoView } from './views/todo';
+import { chartsView } from './views/chart';
+
+function enableRouting() {
+  function setRoute() {
+    $('.view').hide(); // hide - metoda jQuery
+    const { hash } = window.location; // destrukturyzacja globalnego obiektu window.location
+    if (hash === '') {
+      $('#home').show();
+    }
+    $(hash).show(); // show - metoda jQuery
+  }
+  setRoute();
+  window.addEventListener('hashchange', setRoute);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  camelCaseView();
+  todoView();
+  chartsView();
+  enableRouting();
+});
