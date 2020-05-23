6a6285b3e955163666e5d57d93b6c9d541fab0a4
olszowskijacek <jacek.olszowski1@gmail.com>
2020-05-23 12:36:29 +0200

    add progress bars logic

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ const selectType = document.querySelector('select');
 const pokePhoto = document.querySelector('.poke-photo');
 const pokeName = document.querySelector('.poke-name');
 const pokeType = document.querySelector('.type-span');
+const hp = document.querySelector('.hp');
+const speed = document.querySelector('.speed');
+const defense = document.querySelector('.defense');
+const attack = document.querySelector('.attack');
 
-console.log(pokeType);
+console.log(hp);
+// console.log(pokeType);
 
 const number = 50;
 const type = 18;
@@ -42,13 +47,26 @@ for (let i = 0; i < poke.length; i++) {
       .then((res) => res.json())
       .then((res) => {
         pokeName.textContent = res.name;
-        console.log(res);
+        // console.log(res);
         const typeArr = res.types;
         if (typeArr.length === 2) {
           pokeType.textContent = `${typeArr[0].type.name} / ${typeArr[1].type.name}`;
         } else {
           pokeType.textContent = typeArr[0].type.name;
         }
+        const hpBar = res.stats[0].base_stat;
+        const speedBar = res.stats[3].base_stat;
+        const defenseBar = res.stats[4].base_stat;
+        const attackBar = res.stats[5].base_stat;
+
+        hp.style.width = `${hpBar}%`;
+        hp.textContent = hpBar;
+        speed.style.width = `${speedBar}%`;
+        speed.textContent = speedBar;
+        defense.style.width = `${defenseBar}%`;
+        defense.textContent = defenseBar;
+        attack.style.width = `${attackBar}%`;
+        attack.textContent = attackBar;
       });
   });
 }
