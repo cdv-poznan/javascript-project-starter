d44d4a6f826634fba127a93d6860e6ea7309d165
Michał Składanowski <michal.skladanowski@melements.pl>
2020-05-25 23:19:39 +0200

    Now playing section on homepage

--- src/js/homePage.js
+++ src/js/homePage.js
@@ -5,15 +5,18 @@ const homeTemplate = require('../templates/homeTemplate.handlebars');
 
 const queryPopular = 'movie/popular';
 const queryTopRated = 'movie/top_rated';
+const queryNowPlaying = 'movie/now_playing';
 
 const el = document.querySelector('#app');
 const renderHomePage = async () => {
   const resultsPopular = await apiCall(queryPopular);
   const resultsTopRated = await apiCall(queryTopRated);
+  const resultsNowPlaying = await apiCall(queryNowPlaying);
 
   el.innerHTML = homeTemplate({
     popularCarouselContext: { type: 'popular', results: resultsPopular },
     topRatedCarouselContext: { type: 'top_rated', results: resultsTopRated },
+    nowPlayingCarouselContext: { type: 'now_playing', results: resultsNowPlaying },
   });
   new Glide('#popular', {
     type: 'carousel',
@@ -28,6 +31,18 @@ const renderHomePage = async () => {
     },
   }).mount();
   new Glide('#top_rated', {
+    type: 'carousel',
+    perView: 5,
+    breakpoints: {
+      800: {
+        perView: 4,
+      },
+      600: {
+        perView: 2,
+      },
+    },
+  }).mount();
+  new Glide('#now_playing', {
     type: 'carousel',
     perView: 5,
     breakpoints: {
