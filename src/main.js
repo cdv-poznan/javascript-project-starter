546a9e44bd3a69aa49a8e818d99a22603a4345b8
KrystianDerdzinski <derdzinski.krystian@gmail.com>
2020-06-06 16:09:41 +0200

    Fix overdub on playing resume

--- src/main.js
+++ src/main.js
@@ -9,42 +9,56 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#styleChanger').addEventListener('click', () => styles.classList.toggle('hidden'));
   // TECHNO
   function technoSequencer(state) {
-    const kick = new Tone.Player('./assets/samples/techno/technoKick.wav').toMaster();
-    const hat = new Tone.Player('./assets/samples/techno/technoHat.wav').toMaster();
-    const snare = new Tone.Player('./assets/samples/techno/technoSnare.wav').toMaster();
-    const shaker = new Tone.Player('./assets/samples/techno/technoShaker.wav').toMaster();
+    const technoKick = new Tone.Buffer('./assets/samples/techno/technoKick.wav');
+    const technoHat = new Tone.Buffer('./assets/samples/techno/technoHat.wav');
+    const technoSnare = new Tone.Buffer('./assets/samples/techno/technoSnare.wav');
+    const technoShaker = new Tone.Buffer('./assets/samples/techno/technoShaker.wav');
+    let technoKickPlayer = {};
+    let technoHatPlayer = {};
+    let technoSnarePlayer = {};
+    let technoShakerPlayer = {};
+    Tone.Buffer.on('load', function () {
+      technoKickPlayer = new Tone.Player(technoKick);
+      technoKickPlayer.toMaster();
+      technoHatPlayer = new Tone.Player(technoHat);
+      technoHatPlayer.toMaster();
+      technoSnarePlayer = new Tone.Player(technoSnare);
+      technoSnarePlayer.toMaster();
+      technoShakerPlayer = new Tone.Player(technoShaker);
+      technoShakerPlayer.toMaster();
+    });
     let index = 0;
     function repeat() {
       const step = index % 16;
-      const kickInputs = document.querySelector(`.kick input:nth-of-type(${step + 1})`);
-      const hatInputs = document.querySelector(`.hihat input:nth-of-type(${step + 1})`);
-      const snareInputs = document.querySelector(`.snare input:nth-of-type(${step + 1})`);
-      const shakerInputs = document.querySelector(`.shaker input:nth-of-type(${step + 1})`);
+      let kickInputs = [];
+      let hatInputs = [];
+      let snareInputs = [];
+      let shakerInputs = [];
+      kickInputs = document.querySelector(`.kick input:nth-of-type(${step + 1})`);
+      hatInputs = document.querySelector(`.hihat input:nth-of-type(${step + 1})`);
+      snareInputs = document.querySelector(`.snare input:nth-of-type(${step + 1})`);
+      shakerInputs = document.querySelector(`.shaker input:nth-of-type(${step + 1})`);
       if (kickInputs.checked) {
-        kick.start();
+        technoKickPlayer.start();
       }
       if (hatInputs.checked) {
-        hat.start();
+        technoHatPlayer.start();
       }
       if (snareInputs.checked) {
-        snare.start();
+        technoSnarePlayer.start();
       }
       if (shakerInputs.checked) {
-        shaker.start();
+        technoShakerPlayer.start();
       }
       index += 1;
-      console.log(index);
     }
-    Tone.Transport.scheduleRepeat(repeat, '16n');
-    Tone.Transport.start();
+    if (state === 'start') {
+      Tone.Transport.bpm.value = 140;
+      Tone.Transport.scheduleRepeat(repeat, '16n');
+      Tone.Transport.start();
+    }
     if (state === 'stop') {
-      Tone.Transport.stop();
-      // kick.stop();
-      // hat.stop();
-      // snare.stop();
-      // shaker.stop();
-      Tone.Master.dispose();
-      // index = 0;
+      Tone.Transport.cancel();
     }
   }
   function playTechno() {
@@ -57,7 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
   const playBtn = document.querySelector('#play');
-  //  TO DO
   playBtn.addEventListener('click', function () {
     if (technoIsPlayed === true) {
       playBtn.innerHTML = '<i class="icon-play-circled2"></i>';
