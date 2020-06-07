6bc2e8d21db857548a33421a733bc46886271c98
Dawid Eder <dawideder@gmail.com>
2020-06-07 12:30:08 +0200

    First commit

--- src/main.js
+++ src/main.js
@@ -1,83 +1,15 @@
-const container = document.querySelector('.container');
-var inputValue = document.querySelector('.input');
-const add = document.querySelector('.add');
+const jokeBtn = document.getElementById('jokeBtn');
+const jokeText = document.getElementById('jokeText');
 
-if (window.localStorage.getItem('todos') == undefined) {
-  var todos = [];
-  window.localStorage.setItem('todos', JSON.stringify(todos));
-}
-
-var todosEX = window.localStorage.getItem('todos');
-var todos = JSON.parse(todosEX);
-
-class item {
-  constructor(name) {
-    this.createItem(name);
-  }
-  createItem(name) {
-    var itemBox = document.createElement('div');
-    itemBox.classList.add('item');
-
-    var input = document.createElement('input');
-    input.type = 'text';
-    input.disabled = true;
-    input.value = name;
-    input.classList.add('item_input');
-
-    var edit = document.createElement('button');
-    edit.classList.add('edit');
-    edit.innerHTML = 'EDIT';
-    edit.addEventListener('click', () => this.edit(input, name));
-
-    var remove = document.createElement('button');
-    remove.classList.add('remove');
-    remove.innerHTML = 'REMOVE';
-    remove.addEventListener('click', () => this.remove(itemBox, name));
-
-    container.appendChild(itemBox);
-
-    itemBox.appendChild(input);
-    itemBox.appendChild(edit);
-    itemBox.appendChild(remove);
-  }
-
-  edit(input, name) {
-    if (input.disabled == true) {
-      input.disabled = !input.disabled;
-    } else {
-      input.disabled = !input.disabled;
-      let indexof = todos.indexOf(name);
-      todos[indexof] = input.value;
-      window.localStorage.setItem('todos', JSON.stringify(todos));
-    }
-  }
-
-  remove(itemBox, name) {
-    itemBox.parentNode.removeChild(itemBox);
-    let index = todos.indexOf(name);
-    todos.splice(index, 1);
-    window.localStorage.setItem('todos', JSON.stringify(todos));
-  }
-}
-
-add.addEventListener('click', check);
-window.addEventListener('keydown', (e) => {
-  if (e.which == 13) {
-    check();
-  }
+jokeBtn.addEventListener('click', () => {
+  const url = 'https://api.icndb.com/jokes/random';
+  fetch(url)
+    .then((res) => {
+      //response of HTTP in the format of JSON, typical fetch API//
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data.value.joke);
+      jokeText.innerHTML = data.value.joke;
+    });
 });
-
-function check() {
-  if (inputValue.value != '') {
-    new item(inputValue.value);
-    todos.push(inputValue.value);
-    window.localStorage.setItem('todos', JSON.stringify(todos));
-    inputValue.value = '';
-  }
-}
-
-for (var v = 0; v < todos.length; v++) {
-  new item(todos[v]);
-}
-
-// new item('sport');
