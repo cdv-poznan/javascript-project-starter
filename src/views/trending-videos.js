e9061b98f79dbb35d6cee6f796de5bad9808d2ca
gierszewski <gierszewski@hsm.co>
2020-06-08 03:48:11 +0200

    ozliwość zmiany liczby ładownych filmów

--- src/views/trending-videos.js
+++ src/views/trending-videos.js
@@ -1,31 +1,5 @@
 import $ from 'jquery';
 
-function appendVideos(videos) {
-  const trendingYoutubeViedeosWrapper = $('#trending-youtube-videos');
-
-  if (videos.items) {
-    videos.items.map((item) => {
-      trendingYoutubeViedeosWrapper.append(
-        $(`
-        <div class="video-item">
-          <h2>${item.snippet.title}</h2>
-          <div class="video-content">
-            <div><p>${item.snippet.description}</p></div>
-            <div>
-              <iframe type="text/html" width="640" height="360"
-                src="http://www.youtube.com/embed/${item.id}"
-                frameborder="0"/>
-            </div>
-          </div>
-        </div>
-      `),
-      );
-
-      return item;
-    });
-  }
-}
-
 export async function trendingYoutubeView() {
   async function getTrendingVideosForPoland(limit) {
     const API_KEY = 'AIzaSyAuDhPTKYedbL1EGEtERgu9KC-Dj4Et5hg';
@@ -37,7 +11,44 @@ export async function trendingYoutubeView() {
     return json;
   }
 
-  const trendingVideos = await getTrendingVideosForPoland(5);
+  function appendVideos(videos) {
+    const trendingYoutubeViedeosWrapper = $('#trending-youtube-videos');
 
+    // czyścimy zawartość kontenera
+    trendingYoutubeViedeosWrapper.html('');
+
+    if (videos.items) {
+      videos.items.map((item, index) => {
+        trendingYoutubeViedeosWrapper.append(
+          $(`
+          <div class="video-item">
+            <h2>${index + 1}. ${item.snippet.title}</h2>
+            <div class="video-content">
+              <div><p>${item.snippet.description}</p></div>
+              <div>
+                <iframe type="text/html" width="640" height="360"
+                  src="http://www.youtube.com/embed/${item.id}"
+                  frameborder="0"/>
+              </div>
+            </div>
+          </div>
+        `),
+        );
+        return item;
+      });
+    }
+  }
+
+  // domyślnie pobieramy 5 filmów
+  let trendingVideos = await getTrendingVideosForPoland(5);
   appendVideos(trendingVideos);
+
+  async function getTrendingAjax(event) {
+    trendingVideos = await getTrendingVideosForPoland(event.target.value);
+    appendVideos(trendingVideos);
+  }
+
+  // aktualizaujemy listę przy zmianie oczekiwanej liczby ładowanych filmów
+  const videoLimitElement = document.querySelector('#video-limit');
+  videoLimitElement.addEventListener('change', (event) => getTrendingAjax(event), false);
 }
